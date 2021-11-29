77273de2cf0cbf41006122ebf176fe9d5a5527af
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-11-28 20:09:43 -0500

    Create flags for main loop, evenness, and primeness

--- Main.go
+++ Main.go
@@ -3,30 +3,46 @@ package main
 import "fmt"
 
 func main() {
-	for x := 0; x <= 10; x++ {
 
-		//check if divisibility
-		var flag bool = false
+	//controls the program's state
+	var running bool = true
 
-		for i := 2; i < x; i++ {
-			if x%i == 0 {
-				flag = true
-				break
+	var showEvenness bool = true
+
+	var showPrimeness bool = true
+
+	for running {
+
+		for x := 0; x <= 10; x++ {
+
+			if showPrimeness {
+
+				//check divisibility
+				var flag bool = false
+
+				for i := 2; i < x; i++ {
+					if x%i == 0 {
+						flag = true
+						break
+					}
+				}
+
+				if !flag {
+					fmt.Printf("%v is a prime number\n", x)
+				} else {
+					fmt.Printf("%v is not a prime number\n", x)
+				}
 			}
-		}
 
-		if !flag {
-			fmt.Printf("%v is a prime number\n", x)
-		} else {
-			fmt.Printf("%v is not a prime number\n", x)
-		}
+			if showEvenness {
+				if x%2 == 0 {
+					fmt.Printf("%v is even\n", x)
+				} else {
+					fmt.Printf("%v is odd\n", x)
+				}
+			}
 
-		if x%2 == 0 {
-			fmt.Printf("%v is even\n", x)
-		} else {
-			fmt.Printf("%v is odd\n", x)
+			fmt.Println("-----------------------------")
 		}
-
-		fmt.Println("-----------------------------")
 	}
 }
